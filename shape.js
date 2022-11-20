66c4763536d14bdd69a5a56508736ea93f1a33ce
Sneha Hari Khairnar <109480526+khairnarsneha@users.noreply.github.com>
2022-11-20 14:48:27 +0530

    Update shape.js

--- shape.js
+++ shape.js
@@ -1,25 +1,26 @@
 // declare class
+
+
 class Shape {
+   
    constructor() {
+      
     this.color = "green";
   }
+   
   drawShape() {
     
   }
+   
   calculateArea() {
     
   }
+   
+   
+
+
 }  
+
+
 // export class using module.exports
 module.exports = Shape;
-
-
-
-
-
-
-
-
-
-
-
